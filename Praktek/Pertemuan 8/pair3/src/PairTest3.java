0d50c942830e9d7c513893bb76845b2a5ac013cc
jzdnnn <97737462+jzdnnn@users.noreply.github.com>
2024-04-29 07:39:23 +0700

    Add files via upload

---
+++ Praktek/Pertemuan 8/pair3/src/PairTest3.java
@@ -0,0 +1,122 @@
+/**
+ * @version 1.01 2012-01-26
+ * @author Cay Horstmann
+ */
+public class PairTest3 {
+    public static void main(String[] args) {
+        var ceo = new Manager("Gus Greedy", 800000, 2003, 12, 15);
+        var cfo = new Manager("Sid Sneaky", 600000, 2003, 12, 15);
+        var buddies = new Pair<>(ceo, cfo);
+        printBuddies(buddies);
+
+        ceo.setBonus(1000000);
+        cfo.setBonus(500000);
+        Manager[] managers = {ceo, cfo};
+
+        var result = new Pair<>();
+        minmaxBonus(managers, result);
+        System.out.println("first: " + ((Manager)result.getFirst()).getName() + ", second: " + ((Manager)result.getSecond()).getName());
+        maxminBonus(managers, result);
+        System.out.println("first: " + ((Manager)result.getFirst()).getName() + ", second: " + ((Manager)result.getSecond()).getName());
+    }
+
+    public static void printBuddies(Pair<? extends Employee> p) {
+        Employee first = p.getFirst();
+        Employee second = p.getSecond();
+        System.out.println(first.getName() + " and " + second.getName() + " are buddies.");
+    }
+
+    public static void minmaxBonus(Manager[] a, Pair<? super Manager> result) {
+        if (a.length == 0) return;
+        Manager min = a[0];
+        Manager max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.getBonus() > a[i].getBonus()) min = a[i];
+            if (max.getBonus() < a[i].getBonus()) max = a[i];
+        }
+        result.setFirst(min);
+        result.setSecond(max);
+    }
+
+    public static void maxminBonus(Manager[] a, Pair<? super Manager> result) {
+        minmaxBonus(a, result);
+        PairAlg.swapHelper(result); // OK--swapHelper captures wildcard type
+    }
+}
+
+class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
+
+class PairAlg {
+    public static boolean hasNulls(Pair<?> p) {
+        return p.getFirst() == null || p.getSecond() == null;
+    }
+
+    public static void swap(Pair<?> p) {
+        swapHelper(p);
+    }
+
+    public static <T> void swapHelper(Pair<T> p) {
+        T t = p.getFirst();
+        p.setFirst(p.getSecond());
+        p.setSecond(t);
+    }
+}
+
+class Employee {
+    private String name;
+
+    public Employee() {
+    }
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double bonus, int year, int month, int day) {
+        super(name);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+}
